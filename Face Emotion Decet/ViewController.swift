4be87d6dc9974d9ac50e0ea25f6a5a73e75d1cc7
Bekir Duran <akinmihal@gmail.com>
2020-04-27 01:36:57 +0300

    Initial Commit

---
+++ Face Emotion Decet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Face Emotion Decet
+//
+//  Created by Bekir Duran on 27.04.2020.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
